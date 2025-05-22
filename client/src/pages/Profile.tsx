7b5133a5c42a771ba30809547eb214a0e953b353
Colton Blackwell <colton_blackwell@sfu.ca>
2025-05-22 11:29:42 -0700

    Updated front end

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -2,16 +2,23 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 export const Profile = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
 
-    const { user } = useAuth0();
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-    return (
-        <div>
-            <h1>This is the profile page</h1>
-            <p>{user?.name}</p>
-            <p>{user?.picture}</p>
-        </div>
-    )
-}
+  if (!isAuthenticated) {
+    return <div>Please log in to view profile details.</div>;
+  }
+
+  return (
+    <div>
+      <h1></h1>
+      <img src={user?.picture} alt={user?.name} style={{ borderRadius: '50%' }} />
+      <p>Email: {user?.email}</p>
+    </div>
+  );
+};
 
 export default Profile;
